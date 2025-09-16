cca356fcddf88c8d55f19825b6470f5c373ab0d2
Abdumalik <adalerzoda3@gmail.com>
2025-09-16 12:24:47 -0400

    setup

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse('Hello from Shop App')
+    products = Product.objects.all()
+    return render(request, 'products.html', {"products": products})
+
+
+def single_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'single_product.html', {"product": product})
